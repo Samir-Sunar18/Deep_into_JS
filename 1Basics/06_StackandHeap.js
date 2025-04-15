63a3bf3ddd376d50316b59c6db6a7c9c5476802d
Samir-Sunar18 <139344182+Samir-Sunar18@users.noreply.github.com>
2025-04-15 05:52:00 +0000

    filename ch

---
+++ 1Basics/06_StackandHeap.js
@@ -0,0 +1,15 @@
+//Stack and heap memory
+
+// Stack (Primitive), Heap (Non-primitive)
+
+// let youTubename= "SamSmash"
+
+// let anothername = youTubename
+
+// anothername= "SamirSunar"
+
+// console.log(youTubename);
+// console.log(anothername);
+
+console.log("Samir is here in this file");
+
