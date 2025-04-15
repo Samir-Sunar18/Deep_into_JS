4f017c2ebae5262d3602af8dce2ef40ba90957eb
Samir-Sunar18 <139344182+Samir-Sunar18@users.noreply.github.com>
2025-04-15 04:44:48 +0000

    stack and heap memory

--- 1Basics/06_stack&HeapMemory.js
+++ 1Basics/06_stack&HeapMemory.js
@@ -1,2 +1,13 @@
 //Stack and heap memory
 
+// Stack (Primitive), Heap (Non-primitive)
+
+let youTubename= "SamSmash"
+
+let anothername = youTubename
+
+anothername= "SamirSunar"
+
+console.log(youTubename);
+console.log(anothername);
+
