0f075bd3eb1141dc504ce53a56aad9cde17c86e3
Samir-Sunar18 <139344182+Samir-Sunar18@users.noreply.github.com>
2025-06-02 09:00:46 +0000

    maths and numbers

--- 1Basics/08_numbers_maths.js
+++ 1Basics/08_numbers_maths.js
@@ -1,13 +1,52 @@
-// const score =100;
-// console.log(score);
-let balance = new Number(100);
+// // const score =100;
+// // console.log(score);
+// let balance = new Number(100);
+// // console.log(balance);
+
+// // balance = balance.toString();
+// // console.log(balance.toString());
+// console.log(balance.toFixed(2));
 // console.log(balance);
 
-balance = balance.toString();
-// console.log(balance.toString());
-console.log(balance.toFixed (2));
-console.log(balance);
+// // console.log(typeof balance.toFixed());
 
-// console.log(typeof balance.toFixed());
+// const ontherNumber = 2334.8666
+
+// console.log(ontherNumber.toPrecision(3)); // 100.1
+
+// const hundreds = 1000000
+//  console.log(hundreds.toLocaleString('en-US')); 
+ 
+//  console.log(hundreds.toLocaleString('en-US', { style: 'currency', currency: 'USD' })); // $1,000,000.00
+ 
+
+// // +++++++++++++++++++++++  Maths  ++++++++++++++++++++++++
+
+// // console.log(Math);
+// // console.log(Math.abs(32)); //gives absolute value
+// // console.log(Math.ceil(4.1)); // rounds up
+// // console.log(Math.floor(30.99)); // rounds down
+// // console.log(Math.round(32.6)); // rounds to nearest integer
+// // // console.log("This is the maximum value:",Math.max(32, 45, 67, 89, 91)); // returns the maximum value
+
+// // const numbers = [32, 45, 67, 89, 91];
+// // const maxValue = Math.max(...numbers);
+
+// // console.log(`This is the maximum value (${maxValue}) from these numbers: ${numbers.join(", ")}`);
+
+
+// console.log(Math.random()); // returns a random number between 0 and 1
+// console.log((Math.random()*10) +1); // returns a random number between 0 and 1
+// console.log((Math.random()*10) +1); // returns a random number between 0 and 1
+// console.log(Math.floor(Math.random()*10) + 1); // returns a random integer between 1 and 10
+
+
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min +1 )) + min); // returns a random integer between 10 and 20
+ 
 
 
