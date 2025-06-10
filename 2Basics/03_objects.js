2dbad3fa88680c69d55602c948576a5831df0055
Samir-Sunar18 <139344182+Samir-Sunar18@users.noreply.github.com>
2025-06-10 15:01:06 +0000

    object

---
+++ 2Basics/03_objects.js
@@ -0,0 +1,24 @@
+// Objects in Javascript
+// As said if you have to learn javascript , learn Objects and Events in Javascript
+
+// there are two ways of declaring objs in JS
+// ONE as Literal and another as Constructor
+
+// there also comes the concept called : 
+
+//singleton
+// when we create any constructor and an object is created as SingleTon
+
+//Object Literals
+
+
+
+
+
+
+
+
+
+
+
+
