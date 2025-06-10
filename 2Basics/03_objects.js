bd51f87bb5133ee361762fd50d5c43fd4ee2bffa
Samir-Sunar18 <139344182+Samir-Sunar18@users.noreply.github.com>
2025-06-10 15:45:28 +0000

    objects oart 1

--- 2Basics/03_objects.js
+++ 2Basics/03_objects.js
@@ -9,13 +9,50 @@
 //singleton
 // when we create any constructor and an object is created as SingleTon
 
+//when we declarea s object it doesnot become Singleton
+// Object.create //throuigh constructor method
+
+
+
+const mySymb = Symbol("key12")
 //Object Literals
+const user = {
+name :"samir",
+[mySymb] : "Keyshere",  
+"full name" : "Samir Sunar",
+class: 12,
+location: "Lalitpur",
+email : "samsmash@gmail.com",
+isLoggedIn: false,
+lastLoginDays : ["Mondays" ,"Friday"]
+ }
+//  console.log(user.email);
+
+// console.log(user["email"])
+// console.log(user["full name"])
+// console.log(user[mySymb])
+
+// console.log(typeof user.mySymb);
+user.email = "samiringemini@gmail.com"
 
 
+// Object.freeze(user)
+user.email = "samirinchatgpt@gmail.com"
+
+// console.log(user);
 
 
+user.greeting = function (){
+    console.log("Hello JS users");
+    
+}
+console.log(user.greeting());
 
-
+user.greetingTwo = function (){
+    console.log(`Hello JS users, ${this.name}`);
+    
+}
+console.log(user.greetingTwo());
 
 
 
