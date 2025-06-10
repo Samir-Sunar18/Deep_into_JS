2e98d69950326c1c73a0d0e29d570099fd5f7fdc
Samir-Sunar18 <139344182+Samir-Sunar18@users.noreply.github.com>
2025-06-10 17:33:33 +0000

    done all

--- 1Basics/07_String.js
+++ 1Basics/07_String.js
@@ -1,25 +1,25 @@
-// const name = "Samir";
-// const age = 25;
-// const isStudent = true;
-// const greeting = "Hello, my name is " + name + ". I am " + age + " years old.";
-// const greetingTemplate = `Hello, my name is ${name}. I am ${age} years old.`;
-// console.log(greeting);
-// console.log(greetingTemplate);
-// const multilineString = `This is a string
-// that spans multiple lines.`;            
+const name = "Samir";
+const age = 25;
+const isStudent = true;
+const greeting = "Hello, my name is " + name + ". I am " + age + " years old.";
+const greetingTemplate = `Hello, my name is ${name}. I am ${age} years old.`;
+console.log(greeting);
+console.log(greetingTemplate);
+const multilineString = `This is a string
+that spans multiple lines.`;            
 
-// const name = "sam"
-// const Repocount = 23
+const name = "sam"
+const Repocount = 23
 
-// console.log(`Hello my name is ${name} and I have ${Repocount} repositories`);
-
-
-//   const titlename = new String ('Sameer')                      
-// console.log(titlename[2]);
+console.log(`Hello my name is ${name} and I have ${Repocount} repositories`);
 
 
-// console.log(`Hi my name is ${name2}`);
-// console.log(name2.toUpperCase());
+  const titlename = new String ('Sameer')                      
+console.log(titlename[2]);
+
+
+console.log(`Hi my name is ${name2}`);
+console.log(name2.toUpperCase());
 
 console.log(name2.charAt(7));
 console.log(name2.indexOf('r'));
